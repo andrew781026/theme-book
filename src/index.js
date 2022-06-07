c0fd606206fbf25ad80fbb0f8a00259632a356e0
王澍 <andrew7810262000@gmail.com>
2022-06-07 10:30:30 +0800

    move WebIcons to src/assets/fonts

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import GlobalStyle from "./global.style";
+import WebIconStyle from "./fonts.style";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <GlobalStyle/>
+    <WebIconStyle/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
